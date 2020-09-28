nizamial09/ceph

mgr/dashboard: add JUnit reports to tests
Fixes: https://tracker.ceph.com/issues/47118
Signed-off-by: Ernesto Puerta <epuertat@redhat.com>

src/pybind/mgr/dashboard/run-frontend-e2e-tests.sh
@@ -93,6 +93,9 @@ fi
 
 cd $DASH_DIR/frontend
 
+# Remove existing XML results
+rm -f cypress/results/results-*.xml || true
+
 case "$DEVICE" in
     docker)
         failed=0

thotz/ceph-ci

src/pybind/mgr/dashboard/run-frontend-e2e-tests.sh
@@ -93,6 +93,9 @@ fi
 
 cd $DASH_DIR/frontend
 
+# Remove existing XML results
+rm -f cypress/reports/results-*.xml || true
+
 case "$DEVICE" in
     docker)
         failed=0