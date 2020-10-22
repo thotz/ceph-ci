petrutlucian94/ceph

common: accept config files containing whitespaces
At the moment, the ceph config file location cannot contain
whitespaces. This applies to all platforms but is quite an
inconvenient on Windows in particular, where it's very common
to have default paths containing white spaces.

ceph_resolve_file_search expects a list of config files and uses
"get_str_list" to separate them. No delimiter list is explicitly
specified, so the default delimiter list is used, which includes
white space.

This change will explicitly set the delimiters to "," and ";".

Signed-off-by: Lucian Petrut <lpetrut@cloudbasesolutions.com>

src/common/config.cc
@@ -83,7 +83,7 @@ int ceph_resolve_file_search(const std::string& filename_list,
 			     std::string& result)
 {
   list<string> ls;
-  get_str_list(filename_list, ls);
+  get_str_list(filename_list, ";,", ls);
 
   int ret = -ENOENT;
   list<string>::iterator iter;
@@ -373,7 +373,7 @@ int md_config_t::parse_config_files(ConfigValues& values,
   }
 
   std::list<std::string> conf_files;
-  get_str_list(conf_files_str, conf_files);
+  get_str_list(conf_files_str,";,", conf_files);
   auto p = conf_files.begin();
   while (p != conf_files.end()) {
     string &s = *p;

thotz/ceph-ci

src/common/config.cc
@@ -87,7 +87,7 @@ int ceph_resolve_file_search(const std::string& filename_list,
 			     std::string& result)
 {
   list<string> ls;
-  get_str_list(filename_list, ls);
+  get_str_list(filename_list, ";,", ls);
 
   int ret = -ENOENT;
   list<string>::iterator iter;
@@ -440,7 +440,7 @@ md_config_t::get_conffile_paths(const ConfigValues& values,
   }
 
   std::list<std::string> paths;
-  get_str_list(conf_files_str, paths);
+  get_str_list(conf_files_str, ";,", paths);
   for (auto i = paths.begin(); i != paths.end(); ) {
     string& path = *i;
     if (path.find("$data_dir") != path.npos &&