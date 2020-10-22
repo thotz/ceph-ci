athanatos/ceph

test/crimson/seastore/test_transaction_manager: close and reopen tm after mkfs
We'll just require this generally.  Most of the time, mkfs will be a
standalone operation anyway.

Signed-off-by: Samuel Just <sjust@redhat.com>

src/test/crimson/seastore/test_transaction_manager.cc
@@ -101,6 +101,12 @@ struct transaction_manager_test_t : public seastar_test_suite_t {
     return segment_manager->init().safe_then([this] {
       return tm->mkfs();
     }).safe_then([this] {
+      return tm->close();
+    }).safe_then([this] {
+      destroy();
+      static_cast<segment_manager::EphemeralSegmentManager*>(
+	&*segment_manager)->reopen();
+      init();
       return tm->mount();
     }).handle_error(
       crimson::ct_error::all_same_way([] {

thotz/ceph-ci

src/test/crimson/seastore/test_transaction_manager.cc
@@ -101,6 +101,12 @@ struct transaction_manager_test_t : public seastar_test_suite_t {
     return segment_manager->init().safe_then([this] {
       return tm->mkfs();
     }).safe_then([this] {
+      return tm->close();
+    }).safe_then([this] {
+      destroy();
+      static_cast<segment_manager::EphemeralSegmentManager*>(
+	&*segment_manager)->remount();
+      init();
       return tm->mount();
     }).handle_error(
       crimson::ct_error::all_same_way([] {