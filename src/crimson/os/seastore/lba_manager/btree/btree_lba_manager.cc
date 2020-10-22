athanatos/ceph

crimson/os/seastore/lba_manager: add get_physical_extent_if_live
Signed-off-by: Samuel Just <sjust@redhat.com>

src/crimson/os/seastore/lba_manager/btree/btree_lba_manager.cc
@@ -159,10 +159,15 @@ BtreeLBAManager::set_extent(
     });
 }
 
+static bool is_lba_node(extent_types_t type)
+{
+  return type == extent_types_t::LADDR_INTERNAL ||
+    type == extent_types_t::LADDR_LEAF;
+}
+
 static bool is_lba_node(const CachedExtent &e)
 {
-  return e.get_type() == extent_types_t::LADDR_INTERNAL ||
-    e.get_type() == extent_types_t::LADDR_LEAF;
+  return is_lba_node(e.get_type());
 }
 
 btree_range_pin_t &BtreeLBAManager::get_pin(CachedExtent &e)
@@ -403,6 +408,43 @@ BtreeLBAManager::rewrite_extent_ret BtreeLBAManager::rewrite_extent(
   } else {
     return rewrite_extent_ertr::now();
   }
+}
+
+BtreeLBAManager::get_physical_extent_if_live_ret
+BtreeLBAManager::get_physical_extent_if_live(
+  Transaction &t,
+  extent_types_t type,
+  paddr_t addr,
+  laddr_t laddr,
+  segment_off_t len)
+{
+  ceph_assert(is_lba_node(type));
+  return cache.get_extent_by_type(
+    t,
+    type,
+    addr,
+    laddr,
+    len
+  ).safe_then([=, &t](CachedExtentRef extent) {
+    return get_root(t).safe_then([=, &t](LBANodeRef root) {
+      auto lban = extent->cast<LBANode>();
+      return root->lookup(
+	op_context_t{cache, pin_set, t},
+	lban->get_node_meta().begin,
+	lban->get_node_meta().depth).safe_then([=, &t](LBANodeRef c) {
+	  if (c->get_paddr() == lban->get_paddr()) {
+	    return get_physical_extent_if_live_ret(
+	      get_physical_extent_if_live_ertr::ready_future_marker{},
+	      lban);
+	  } else {
+	    cache.drop_from_cache(lban);
+	    return get_physical_extent_if_live_ret(
+	      get_physical_extent_if_live_ertr::ready_future_marker{},
+	      CachedExtentRef());
+	  }
+	});
+    });
+  });
 }
 
 BtreeLBAManager::BtreeLBAManager(

thotz/ceph-ci

src/crimson/os/seastore/lba_manager/btree/btree_lba_manager.cc
@@ -159,10 +159,15 @@ BtreeLBAManager::set_extent(
     });
 }
 
+static bool is_lba_node(extent_types_t type)
+{
+  return type == extent_types_t::LADDR_INTERNAL ||
+    type == extent_types_t::LADDR_LEAF;
+}
+
 static bool is_lba_node(const CachedExtent &e)
 {
-  return e.get_type() == extent_types_t::LADDR_INTERNAL ||
-    e.get_type() == extent_types_t::LADDR_LEAF;
+  return is_lba_node(e.get_type());
 }
 
 btree_range_pin_t &BtreeLBAManager::get_pin(CachedExtent &e)
@@ -403,6 +408,43 @@ BtreeLBAManager::rewrite_extent_ret BtreeLBAManager::rewrite_extent(
   } else {
     return rewrite_extent_ertr::now();
   }
+}
+
+BtreeLBAManager::get_physical_extent_if_live_ret
+BtreeLBAManager::get_physical_extent_if_live(
+  Transaction &t,
+  extent_types_t type,
+  paddr_t addr,
+  laddr_t laddr,
+  segment_off_t len)
+{
+  ceph_assert(is_lba_node(type));
+  return cache.get_extent_by_type(
+    t,
+    type,
+    addr,
+    laddr,
+    len
+  ).safe_then([=, &t](CachedExtentRef extent) {
+    return get_root(t).safe_then([=, &t](LBANodeRef root) {
+      auto lba_node = extent->cast<LBANode>();
+      return root->lookup(
+	op_context_t{cache, pin_set, t},
+	lba_node->get_node_meta().begin,
+	lba_node->get_node_meta().depth).safe_then([=, &t](LBANodeRef c) {
+	  if (c->get_paddr() == lba_node->get_paddr()) {
+	    return get_physical_extent_if_live_ret(
+	      get_physical_extent_if_live_ertr::ready_future_marker{},
+	      lba_node);
+	  } else {
+	    cache.drop_from_cache(lba_node);
+	    return get_physical_extent_if_live_ret(
+	      get_physical_extent_if_live_ertr::ready_future_marker{},
+	      CachedExtentRef());
+	  }
+	});
+    });
+  });
 }
 
 BtreeLBAManager::BtreeLBAManager(