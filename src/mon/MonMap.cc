ceph/ceph

mon: make mon summary more concise in 'ceph -s'
Signed-off-by: Sage Weil <sage@redhat.com>

src/mon/MonMap.cc
@@ -356,6 +356,14 @@ void MonMap::dump(Formatter *f) const
   f->close_section();
 }
 
+void MonMap::dump_summary(Formatter *f) const
+{
+  f->dump_unsigned("epoch", epoch);
+  f->dump_string("min_mon_release_name", ceph::to_string(min_mon_release));
+  f->dump_unsigned("num_mons", ranks.size());
+}
+
+
 // an ambiguous mon addr may be legacy or may be msgr2--we aren' sure.
 // when that happens we need to try them both (unless we can
 // reasonably infer from the port number which it is).

thotz/ceph-ci

(cherry picked from commit 3e7c185bd4b2312f9801b8664fb175801c904871)
Conflicts:
	PendingReleaseNotes
- drop already published release notes
        src/mon/MonMap.cc
- change ceph::to_string to std::to_string

src/mon/MonMap.cc
@@ -360,6 +360,14 @@ void MonMap::dump(Formatter *f) const
   f->close_section();
 }
 
+void MonMap::dump_summary(Formatter *f) const
+{
+  f->dump_unsigned("epoch", epoch);
+  f->dump_string("min_mon_release_name", std::to_string(min_mon_release));
+  f->dump_unsigned("num_mons", ranks.size());
+}
+
+
 // an ambiguous mon addr may be legacy or may be msgr2--we aren' sure.
 // when that happens we need to try them both (unless we can
 // reasonably infer from the port number which it is).