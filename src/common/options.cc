batrick/ceph

osdc: add timeout configs for mons/osds
The Monitors may not respond to invalid requests like statfs on a
non-existent pool. In this case, the Objecter should eventually give up
rather than block forever.

Fixes: https://tracker.ceph.com/issues/47734
Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

Conflicts:
	src/osdc/Objecter.cc

src/common/options.cc
@@ -3844,6 +3844,18 @@ std::vector<Option> get_global_options() {
     .set_default(false)
     .set_description(""),
 
+    Option("osdc_mon_timeout", Option::TYPE_SECS, Option::LEVEL_ADVANCED)
+    .set_default(5_min)
+    .set_description("timeout for operations handled by monitors such as statfs")
+    .set_flag(Option::FLAG_RUNTIME)
+    .set_min(0),
+
+    Option("osdc_osd_timeout", Option::TYPE_SECS, Option::LEVEL_ADVANCED)
+    .set_default(6_hr)
+    .set_description("timeout for operations handled by osds such as write")
+    .set_flag(Option::FLAG_RUNTIME)
+    .set_min(0),
+
     Option("osd_discard_disconnected_ops", Option::TYPE_BOOL, Option::LEVEL_ADVANCED)
     .set_default(true)
     .set_description(""),

thotz/ceph-ci

Have the Objecter track the rados_(mon|osd)_op_timeout configs so that
it can be configured at runtime/startup. This is useful for the
MDS/ceph-fuse so that we can avoid waiting forever for a response from
the Monitors that will never come (statfs on a deleted file system's
pools).
Also: make these configs take a time value rather than double. This is
simpler to deal with in the code and allows time units to be used (e.g.
"5m" for 5 minutes).
(cherry picked from commit a8a23747aa081d938c9b277ab42507dd506bf6c2)
	src/client/Client.cc
	src/librados/RadosClient.cc
	src/mds/MDSRank.cc
	src/mgr/MgrStandby.cc
	src/mon/MonClient.h
	src/neorados/RADOSImpl.cc
	src/osd/OSD.cc
	src/osdc/Objecter.h
	src/test/mon/test_mon_workloadgen.cc
	src/tools/cephfs/MDSUtility.cc
        Notes: different Objecter cons arguments. Added conf obs for
        RadosClient.

src/common/options.cc
@@ -5145,13 +5145,17 @@ std::vector<Option> get_global_options() {
     .set_default("/tmp/fio")
     .set_description(""),
 
-    Option("rados_mon_op_timeout", Option::TYPE_FLOAT, Option::LEVEL_ADVANCED)
+    Option("rados_mon_op_timeout", Option::TYPE_SECS, Option::LEVEL_ADVANCED)
     .set_default(0)
-    .set_description(""),
+    .set_description("timeout for operations handled by monitors such as statfs (0 is unlimited)")
+    .set_flag(Option::FLAG_RUNTIME)
+    .set_min(0),
 
-    Option("rados_osd_op_timeout", Option::TYPE_FLOAT, Option::LEVEL_ADVANCED)
+    Option("rados_osd_op_timeout", Option::TYPE_SECS, Option::LEVEL_ADVANCED)
     .set_default(0)
-    .set_description(""),
+    .set_description("timeout for operations handled by osds such as write (0 is unlimited)")
+    .set_flag(Option::FLAG_RUNTIME)
+    .set_min(0),
 
     Option("rados_tracing", Option::TYPE_BOOL, Option::LEVEL_ADVANCED)
     .set_default(false)